e442bab929c8144015f008b91798cc66d696f658
CreepyAmateur <58887042+CreepyAmateur@users.noreply.github.com>
2020-05-11 18:29:39 +0200

    Assets

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float timeCounter = 10f;
+
+    public float speed = 5f;
+    public float width = 4f;
+    public float height = 7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += Time.deltaTime*speed;
+
+        float x = Mathf.Cos(timeCounter)*width;
+        float y = Mathf.Sin(timeCounter)*height;
+        float z = 7;
+
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10f;
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), (Input.GetAxis("Vertical")));
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        if(Input.GetKey("w"))
+        {
+            rb.AddForce(direction * speed);
+        }
+    }
+}
