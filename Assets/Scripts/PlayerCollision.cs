c53ec1270349bdc77e1acd10ad1bb7c04038bdc6
CreepyAmateur <58887042+CreepyAmateur@users.noreply.github.com>
2020-05-18 13:35:19 +0200

    Respawn time delay

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,15 +1,52 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
+    public float delay = 3f;
+    float countdown;
+    bool Collided = false;
+
+    private void Start()
+    {
+        countdown = delay;
+    }
+
+    private void Update()
+    {
+        if (Collided)
+        {
+            movement.enabled = false;
+            countdown -= Time.deltaTime;
+            if (countdown <= 0f)
+            {
+                SceneManager.LoadScene("SampleScene");
+                
+            }
+            
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Bullet")
         {
-            movement.enabled = (false);
-            SceneManager.LoadScene("SampleScene");
+            Collided = true;
+                      
         }
     }
+
+    /*private void StartCoroutine(IEnumerable enumerable)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerable WaitBeforeRestart()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("SampleScene");
+    } */
 }
